bb650daec5ef9d874ab1316073959af325bd973a
param0207 <chaudharyparam0207@gmail.com>
2024-11-29 18:59:16 +0530

    add feature for expand-collapse all

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const expandBtn = document.querySelectorAll(".expand-btn");
+const expandAll = document.querySelector(".expand-all");
 
 expandBtn.forEach((btn) => {
   btn.addEventListener("click", (e) => {
@@ -14,4 +15,27 @@ expandBtn.forEach((btn) => {
       cardBody.classList.remove("show");
     }
   });
+});
+
+// add eventlistener on expand all btn
+expandAll.addEventListener("click", (e) => {
+  const target = e.target;
+
+  expandBtn.forEach((btn) => {
+    const parent = btn.closest(".card");
+
+    const cardBody = parent.querySelector(".card-body");
+    if (target.innerText === "Expand All") {
+      btn.innerText = "Collapse";
+      cardBody.classList.add("show");
+    } else {
+      btn.innerText = "Expand";
+      cardBody.classList.remove("show");
+    }
+  });
+  if (target.innerText === "Expand All") {
+    target.innerText = "Collapse All";
+  } else {
+    target.innerText = "Expand All";
+  }
 });
